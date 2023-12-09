c15c898c1c34551ec688cfd65e951b9232d6cdd2
Md Taufeeq Hussain <114144690+taufeeqhussain07@users.noreply.github.com>
2023-12-09 12:10:33 +0530

    Create MatrixAddition.java

---
+++ MatrixAddition.java
@@ -0,0 +1,13 @@
+class MatrixAddition{  
+public static void main(String args[]){  
+int a[][]={{1,3},{2,4}};    
+int b[][]={{1,4},{2,3}};  
+int c[][]=new int[2][2];  
+for(int i=0;i<2;i++){    
+for(int j=0;j<2;j++){    
+c[i][j]=a[i][j]+b[i][j]; 
+System.out.print(c[i][j]+" ");    
+}    
+System.out.println();    
+}    
+}}  
