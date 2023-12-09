e1e8bbaf8a2e8ed43b10c03664a67acf63cb31da
Md Taufeeq Hussain <114144690+taufeeqhussain07@users.noreply.github.com>
2023-12-09 12:14:54 +0530

    Create reverseaNumber.java

---
+++ reverseaNumber.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class ReverseNumber{  
+public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    System.out.print("Enter a number:");
+    int number=sc.nextInt();
+    int reverse = 0;  
+while(number != 0)   
+{  
+int remainder = number % 10;  
+reverse = reverse * 10 + remainder;  
+number = number/10;  
+}  
+System.out.println("The reverse of the given number is: " + reverse);  
+}  
+}  
